d1b427a02758c56e84d35a92234ae05ea2f5f46a
Breno Araripe <brenoma@live.com>
2023-01-03 13:21:19 -0300

    Little wing

--- main.tf
+++ main.tf
@@ -1,32 +1,44 @@
 resource "aws_key_pair" "aws-key" {
-  key_name = "aws_key"
+  key_name   = "aws_key"
   public_key = var.PUBLIC_KEY_SSH
 }
 
 resource "aws_instance" "nginx_server" {
-  ami = "ami-08c40ec9ead489470"
-  instance_type = "t2.micro"
-  subnet_id = aws_subnet.subnets[0].id
+  ami                    = "ami-08c40ec9ead489470"
+  instance_type          = "t2.micro"
+  subnet_id              = aws_subnet.subnets[0].id
   vpc_security_group_ids = ["${aws_security_group.sg.id}"]
-  key_name = aws_key_pair.aws-key.id
+  key_name               = aws_key_pair.aws-key.id
 
   provisioner "file" {
-    source = "nginx.sh"
+    source      = "/scripts/nginx.sh"
     destination = "/tmp/nginx.sh"
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = var.EC2_USER
+      private_key = file(var.PRIVATE_KEY_PATH)
+    }
   }
 
   provisioner "remote-exec" {
     inline = [
       "chmod +x /tmp/nginx.sh",
-      "sudo /tmp/nginx.sh"
+      "sudo /tmp/nginx.sh",
     ]
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = var.EC2_USER
+      private_key = file(var.PRIVATE_KEY_PATH)
+    }
   }
 
   connection {
-    type = "ssh"
-    host = self.public_ip
-    user = var.EC2_USER
-    private_key = var.PRIVATE_KEY_PATH
+    type        = "ssh"
+    host        = self.public_ip
+    user        = var.EC2_USER
+    private_key = file(var.PRIVATE_KEY_PATH)
   }
 
   tags = {
--- providers.tf
+++ providers.tf
@@ -1,13 +1,13 @@
 terraform {
   required_version = ">=0.13.1"
   required_providers {
-    aws = ">=4.40.0"
+    aws   = ">=4.40.0"
     local = ">=2.2.3"
   }
 }
 
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
   access_key = var.AWS_ACCESS_KEY
   secret_key = var.AWS_SECRET_KEY
 }
